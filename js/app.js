9d9431316a238932469f52d797859912d12ec70d
Corey Sokol <Corey_Sokol@icloud.com>
2018-02-11 10:41:30 -0600

    Shuffled array with Fisher-Yates

--- js/app.js
+++ js/app.js
@@ -1,13 +1,35 @@
 let cards = document.querySelector('#container');
-let icons = [1, 2, 3, 4, 5, 6, 7, 8];
+let icons = ["..img/computer-monitor.png", 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
 
+// Using the Fisher-Yates shuffle algorithm to shuffle items in icons array
+function shuffle(arr) {
+  var m = arr.length, temp, i;
+
+  // While there remain elements to shuffle…
+  while (m) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    temp = arr[m];
+    arr[m] = arr[i];
+    arr[i] = temp;
+  }
+
+  return arr;
+}
+// When the DOM loads cycle through the random array and display
+// the random indexes in created div elements
 document.addEventListener("DOMContentLoaded", function() {
-
-  for (let i = 1; i <= 16; i++) {
-    icons.sort(function() {return 0.5 - Math.random});
-    let randomIcons = icons[Math.floor(Math.random() * icons.length)];
+  shuffle(icons);
+  for (let i = 0; i < 16; i++) {
     let memoryCard = document.createElement('div');
-    memoryCard.textContent = randomIcons;
+    memoryCard.innerText = icons[i];
     cards.appendChild(memoryCard);
   }
 });
+
+memoryCard.addEventListener('click', function(e) {
+  return e;
+})
