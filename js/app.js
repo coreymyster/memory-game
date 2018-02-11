d85ac9ad9162b85ea919afe1fdffd1377f2c74f7
Corey <corey_sokol@icloud.com>
2018-02-11 09:11:46 -0600

    Added grid to page on page load

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 let cards = document.querySelector('#container');
+let icons = [1, 2, 3, 4, 5, 6, 7, 8];
 
 document.addEventListener("DOMContentLoaded", function() {
+
   for (let i = 1; i <= 16; i++) {
-    cards.appendChild(document.createElement('div'));
+    icons.sort(function() {return 0.5 - Math.random});
+    let randomIcons = icons[Math.floor(Math.random() * icons.length)];
+    let memoryCard = document.createElement('div');
+    memoryCard.textContent = randomIcons;
+    cards.appendChild(memoryCard);
   }
 });
